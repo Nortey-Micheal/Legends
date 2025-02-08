699f4fa6e8dd48b7d17a812802d8adbde9f82918
Nortey_Micheal <46892240+Nortey-Micheal@users.noreply.github.com>
2025-02-08 17:44:51 +0000

    Update main.tsx. Include lazy loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import NoPagaeFound from './pages/404nopage.tsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
-import ProfilePage from './pages/profilePage.tsx'
+
+//Lazy load pages
+const App = lazy(() => import("./pages/App.tsx"));
+const NoPageFound = lazy(() => import("./pages/404nopage.tsx"));
+const ProfilePage = lazy(() => import("./pages/profilePage.tsx"));
 
 const router = createBrowserRouter([
   {
@@ -24,7 +26,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <StrictMode>
-      <RouterProvider router={router}/>
+      <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
     </StrictMode>
   </Provider>,
 )
